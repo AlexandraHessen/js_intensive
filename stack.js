34c43ab314c2f7ed12d4e712a06f704e7461a478
AlexandraHessen <xl_energy@mail.ru>
2022-04-28 23:23:19 +0300

    task_2_stack add check

--- stack.js
+++ stack.js
@@ -1,12 +1,15 @@
+"use strict";
 class Stack {
     constructor() {
         this.stack = [];
         this.size = 0;
     }
+
     push(item) {
         this.stack[this.size] = item;
         this.size++
     }
+
     pop() {
         if (this.size === 0) {
             return undefined;
@@ -26,3 +29,18 @@ class Stack {
         return this.size;
     }
 }
+
+
+let testStack = new Stack();
+testStack.push(10);
+testStack.push(20);
+testStack.push(30);
+testStack.push(40);
+console.log(testStack); // testStack = [10, 20, 30, 40]
+
+console.log(testStack.pop()); //40
+console.log(testStack.pop()); //30
+console.log(testStack); // testStack = [10, 20]
+
+console.log(testStack.peek()); //20
+console.log(testStack.length()); //2
