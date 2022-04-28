db773c56287faf504e9969d01a85da283cf89b8b
AlexandraHessen <xl_energy@mail.ru>
2022-04-28 23:35:12 +0300

    task_1 add check

--- myFilter.js
+++ myFilter.js
@@ -1,3 +1,4 @@
+"use strict";
 Array.prototype.myFilter = function (callback, thisArg) {
     if (!(Array.isArray(this))) {
         throw new Error("Невалидное значение массива!")
@@ -18,3 +19,30 @@ Array.prototype.myFilter = function (callback, thisArg) {
     return resultArr;
 }
 
+
+
+//check
+const arr = [1, -9, 0, 11];
+
+function callbackToCheck(num) {
+    return num > 0;
+}
+
+console.log(arr.myFilter(callbackToCheck)); //[1, 11]
+
+
+const data = [8, "44", "red", 11, 58, 145, 6];
+const range = {
+    lower: 0,
+    upper: 100
+};
+
+function isInRange(value) {
+    if (typeof value !== 'number') {
+        return false;
+    }
+    return value >= this.lower && value <= this.upper;
+}
+
+const numberInRange = data.myFilter(isInRange, range);
+console.log(numberInRange); // [8, 11, 58, 6]
